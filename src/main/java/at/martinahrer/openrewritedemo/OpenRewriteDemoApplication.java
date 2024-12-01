9ec26c9bb381d698e37d8e2fa8db33432158766c
Martin Ahrer <this@martinahrer.at>
2024-12-01 17:14:27 +0100

    Add open-rewrite

---
+++ src/main/java/at/martinahrer/openrewritedemo/OpenRewriteDemoApplication.java
@@ -0,0 +1,13 @@
+package at.martinahrer.openrewritedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenRewriteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenRewriteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/martinahrer/openrewritedemo/OpenRewriteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.martinahrer.openrewritedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRewriteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
